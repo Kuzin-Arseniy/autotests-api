eb431d316b58bb4e960ed4f387e22c9d5695cc00
Kuzin-Arseniy <kuzin.arsenij@icloud.com>
2026-02-20 18:05:46 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,11 @@
+def test_first_try():
+    print("Hello world!")
+
+
+def test_assert_positive_case():
+    assert 1 == 1
+
+
+def test_assert_negative_case():
+    x, y = 1, -1
+    assert x == y, "1 is not equal to -1"
