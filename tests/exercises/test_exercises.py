9c971871586c2187507f20beba55ef2e0ad261b8
Kuzin-Arseniy <kuzin.arsenij@icloud.com>
2026-03-10 18:14:46 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,11 +2,12 @@ from http import HTTPStatus
 import pytest
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseResponseSchema, UpdateExerciseRequestSchema
 from fixtures.courses import CoursesFixtures
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
+    assert_update_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -29,7 +30,6 @@ class TestExercises:
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-
     def test_get_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
         # Отправляем запрос на получение данных курса
         response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
@@ -40,6 +40,25 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         # Проверяем, что данные в ответе на получение задания соответствуют запросу его создание
         assert_get_exercise_response(response_data, function_exercise.response)
+
+        # Валидируем JSON-схему ответа
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_update_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
+        # Описываем данные запроса на обновление данных задания
+        request = UpdateExerciseRequestSchema()
+        # Отправляем запрос на обновление задания с данными по умолчанию, описанными в UpdateExerciseRequestSchema
+        response = exercises_client.update_exercise_api(
+            exercise_id=function_exercise.response.exercise.id,
+            request=request,
+        )
+        # Преобразуем JSON-ответ в объект схемы, для дальнейшей валидации JSON-схемы
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус-код ответа
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # Проверяем, что данные в ответе на обновление задания соответствуют запросу
+        assert_update_exercise_response(request, response_data)
 
         # Валидируем JSON-схему ответа
         validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,5 @@
 from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExerciseRequestSchema, \
-    ExerciseSchema, GetExerciseResponseSchema
+    ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -45,3 +45,20 @@ def assert_get_exercise_response(
     :raises AssertionError: Если хотя бы одно поле не совпадает
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+
+def assert_update_exercise_response(
+        request: UpdateExerciseRequestSchema,
+        response: UpdateExerciseResponseSchema
+):
+    """
+    Функция проверяет, что данные ответа на обновление задания соответствуют данным запроса
+    :param request: Данные запроса на обновление задания
+    :param response: Ответ API на обновление задания
+    :raises AssertionError: Если хотя бы одно поле не совпадает
+    """
+    assert_equal(response.exercise.title, request.title, "title")
+    assert_equal(response.exercise.max_score, request.max_score, "max_score")
+    assert_equal(response.exercise.min_score, request.min_score, "min_score")
+    assert_equal(response.exercise.order_index, request.order_index, "order_index")
+    assert_equal(response.exercise.description, request.description, "description")
+    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
